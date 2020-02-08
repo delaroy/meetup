92ff1bde9e07e2e75e0cf6f0888b1b529eae6a3d
delaroystudios <RepubliC7891>
2020-02-08 13:48:52 +0100

    set up splash, login and register

--- app/src/main/java/com/bamideleoguntuga/meetup/MainActivity.java
+++ app/src/main/java/com/bamideleoguntuga/meetup/MainActivity.java
@@ -3,12 +3,19 @@ package com.bamideleoguntuga.meetup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.product);
     }
 }
